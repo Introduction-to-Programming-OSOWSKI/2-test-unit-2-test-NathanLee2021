ae4a87ff17ae398bf5b90680bec523bdf3ba6344
NathanLee2021 <818998@mystma.org>
2021-01-15 16:20:27 +0000

    test #2

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-#WRITE YOUR CODE IN THIS FILE
+def waterState(f):
+    if f<= 32:
+        return "solid"
+    elif 32 < f < 212:
+        return "liquid"
+    else:
+        return "gas"
+def isDozen (d):
+    if d % 12 == 0:
+        return True
+    else:
+        return False
+def toGerman(a):
+    if a == "yes":
+        return "ja"
+    else:
+        return "nein"
+def stopLight(c):
+    if c == "green":
+        return "go"
+    elif c == "yellow":
+        return "slow"
+    else:
+        return "stop"
